4e563d0ae03b839f1ac8ef1c3c47ca375dcd120e
Brandon Omondi <73080323+brandon-omondi@users.noreply.github.com>
2024-04-10 16:49:03 +0300

    Initial Commit

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 function SearchBar(props) {
     const [jobCriteria, setJobCriteria] = useState({
         title: "",
-        locaion: "",
+        location: "",
         experience: "",
         type:""
     })
@@ -24,30 +24,30 @@ function SearchBar(props) {
     <div className='flex gap-4 my-10 justify-center px-10'>
         <select onChange={handleChange} name="title" value={jobCriteria.title} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
             <option value="" disabled hidden>Job Role</option>
-            <option value="iOS Developer">iOS Developer</option>
-            <option value="Frontend Developer">Frontend Developer</option>
-            <option value="Backend Developer">Backend Developer</option>
-            <option value="Android Developer">Android Developer</option>
-            <option value="Developer Advocate">Developer Advocate</option>
+            <option value="Plumber">Plumber</option>
+            <option value="Laundry Service">Laundry Service</option>
+            <option value="Gardener">Gardener</option>
+            <option value="Baby Sitter">Baby Sitter</option>
+            <option value="Private Chef">Private Chef</option>
         </select>
         <select onChange={handleChange} name="type" value={jobCriteria.type} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
             <option value="" disabled hidden>Job Type</option>
-            <option value="Full Time">Full Time</option>
-            <option value="Part Time">Part Time</option>
+            <option value="One Day">One Day</option>
+            <option value="Weekly">Weekly</option>
+            <option value="Fulltime">Fulltime</option>
             <option value="Contract">Contract</option>
         </select>
         <select onChange={handleChange} name="location" value={jobCriteria.location} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
-            <option value="" disabled hidden>Location</option>
+            <option value="" disabled selected hidden>Location</option>
             <option value="Remote">Remote</option>
-            <option value="In-Office">In-Office</option>
-            <option value="Hybrid">Hybrid</option>
+            <option value="In-Office">On-Location</option>
         </select>
         <select onChange={handleChange} name="experience" value={jobCriteria.experience} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
             <option value="" disabled hidden>Experience</option>
-            <option value="Fresher">Fresher</option>
             <option value="Junior Level">Junior Level</option>
             <option value="Mid Level">Mid Level</option>
-            <option value="Senior Level">Senior Level</option>
+            <option value="Expert">Expert</option>
+            <option value="All experience levels">All experience levels</option>
         </select>
         <button onClick={search} className='w-64 bg-blue-500 text-white font-bold py-3 rounded-md'>Search</button>
     </div>
